2e4001c8ffb550044da1de835de2c95ab2c9748a
r_samohval <rsamohvwal@gmail.com>
2022-01-17 11:51:04 +0300

    использование валидации форм

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,7 @@
 import {ApplicationRef, Component, OnInit} from '@angular/core';
 import {Model} from "../model/repository.model";
 import {Product} from "../model/product.model";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'product',
@@ -29,5 +30,45 @@ export class ProductComponent {
     console.log("New Product: " + this.jsonProduct);
   }
 
+  getValidationMessages(state: any, thingName?: string) {
+    let thing: string = state.path || thingName;
+    let messages: string[] = [];
+    if (state.errors) {
+      for (let errorName in state.errors) {
+        switch (errorName) {
+          case "required":
+            messages.push(`You must enter a ${thing}`);
+            break;
+          case "minlength":
+            messages.push(`A ${thing} must be at least
+             ${state.errors['minlength'].requiredLength} characters`);
+            break;
+          case "pattern":
+            messages.push(`The ${thing} contains illegal characters`);
+            break;
+        }
+      }
+    }
+    return messages;
+  }
 
+  getFormValidationMessages(form: NgForm): string[] {
+    let messages: string[] = [];
+    Object.keys(form.controls).forEach(k => {
+      this.getValidationMessages(form.controls[k], k).forEach(m => messages.push(m));
+    });
+    return messages;
+  }
+
+  formSubmitted: boolean = false;
+
+  submitForm(form: NgForm) {
+    this.formSubmitted = true;
+    if (form.valid) {
+      this.addProduct(this.newProduct);
+      this.newProduct = new Product();
+      form.reset();
+      this.formSubmitted = false;
+    }
+  }
 }
